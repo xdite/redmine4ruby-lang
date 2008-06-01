047913b49040b222e7a5a2690ff939584e8b4fda
Yuki Sonoda (Yugui) <yugui@yugui.jp>
2008-06-01 23:09:17 +0900

    added hyperlink feature to RDFormatting

--- lib/redmine/rd_formatting.rb
+++ lib/redmine/rd_formatting.rb
@@ -12,6 +12,23 @@ module Redmine
       def apply_to_DocumentElement(element, content)
         content.join
       end
+      def apply_to_RefToElement(element, content)
+        content = content.join("")
+        if anchor = refer(element)
+          content = content.sub(/^function#/, "")
+          %Q[<a href="\##{anchor}">#{content}</a>]
+        else
+          label = element.to_label
+          case label
+          when /\A[\w_-]+\z/
+            %Q[<a href="#{label}">#{meta_char_escape(content)}</a>]
+          else
+            label = hyphen_escape(element.to_label)
+            %Q[<!-- Reference, RDLabel "#{label}" doesn't exist -->] +
+             %Q[<em class="label-not-found">#{content}</em><!-- Reference end -->]
+          end
+        end
+      end
     end
     def self.to_html(text, options = {}, &block)
       visitor = RestrictedHTMLVisitor.new
